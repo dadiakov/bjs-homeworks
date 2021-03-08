5f67c59736fa58c44b9637ca3b36d983b98b7924
Dmitry Diakov <d.diakov@list.ru>
2021-03-08 20:03:06 +0500

    All done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,29 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+String.prototype.isPalindrome = function() {
+  let str = this.toLowerCase().replace(/\s/g, '');
+  return str == str.split('').reverse().join('');
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let average = 0;
+  let averageMark = 0;
+  for (let i = 0; i < marks.length; i++) {
+    average += marks[i];
+  }
+  if (!average) {
+    return 0;
+  }
+  return averageMark = Math.round(average / marks.length);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Date.now();
+  let currentYear = new Date().getFullYear();
+  birthday = (new Date(birthday)).getTime();
+  let diff = now - birthday;
+  let isLeap = new Date(currentYear, 1, 29).getMonth() == 1;
+  let age = isLeap? diff / 3.16224e+10 : diff / 3.1536e+10;
+  let verdict = age > 18? true : false;
+  return verdict;
 }
