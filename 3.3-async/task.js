b67d30559b52c408743e7d32463fa66146e24de1
dadiakov <74093790+dadiakov@users.noreply.github.com>
2021-03-22 16:49:33 +0500

    Final fix
    Final fix

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -6,7 +6,7 @@ class AlarmClock {
     this.timerId = null;
   }
   addClock(time, callBack, id) {
-    if(!(id || id==0)) {throw new Error('error text')};
+    if(!(id || id==0)) {throw new Error('Невозможно идентифицировать будильник')};
     
     try { if(this.alarmCollection.some( obj => obj.id == id) ) throw new Error('Такой таймер существует')}
     catch(e) {
@@ -29,29 +29,43 @@ class AlarmClock {
       }
               
   start() {
-    function checkClock(obj) {
-      let currentTime = new Date();
-      let hour = currentTime.getHours();
-      if (hour < 10) hour = '0' + hour;
-      let minute = currentTime.getMinutes();
-      if (minute < 10) minute = '0' + minute;
-      let now = hour + ':' + minute;
+      let checkClock = (obj) => {
+      let now = this.getCurrentFormattedTime();
       if ( obj.time == now ) obj.callBack();
     }
       let interval = 0;
-      if (!this.timerId) interval = 1000;
-      this.timerId = setInterval(this.alarmCollection.map(checkClock), interval);
+      if (!this.timerId) { 
+        interval = 1000;
+        this.timerId = setInterval(() => this.alarmCollection.forEach(checkClock), interval);
+      }
   }
+
   stop() {
-    if(this.timerId) clearInterval(timerId);
+    if(this.timerId) clearInterval(this.timerId);
     this.timerId = null;
   }
+
   printAlarms() {
+    console.log(`Печать всех будильников в количестве: ${this.alarmCollection.length}\n`);
     this.alarmCollection.forEach(function(timer) {
       console.log(`Будильник ${timer.id} заведен на ${timer.time}`)});
   }
+
   clearAlarms() {
     clearInterval(this.timerId);
     this.alarmCollection = [];
   }
 }
+
+function testAlarms() {
+  let phoneAlarm = new AlarmClock();
+  phoneAlarm.addClock('14:39', () => console.log('Пора вставать'), 1);
+  phoneAlarm.addClock('14:40', () => { console.log('Вставай уже!'); phoneAlarm.removeClock(2)}, 2);
+  try {phoneAlarm.addClock('14:40', () => console.log('Пора умываться'));} catch(e) {console.log(e);};
+  phoneAlarm.addClock('14:41', () => {console.log('Вставай, а то проспишь'); phoneAlarm.clearAlarms(); phoneAlarm.printAlarms();}, 3);
+  phoneAlarm.addClock('14:42', () => console.log('Вставай, а то проспишь!'), 1);  
+  phoneAlarm.printAlarms();
+  phoneAlarm.start();
+}
+
+testAlarms();
